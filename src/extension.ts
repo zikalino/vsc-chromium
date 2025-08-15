9a3ebd5df0e3b3397882eb3dc95ab00f83bd1678
Zim Kalinowski <zkalinowski@opera.com>
2025-08-15 15:40:18 +0800

    displaying process info

--- src/extension.ts
+++ src/extension.ts
@@ -39,4 +39,5 @@ async function displayChromiumExplorer(extensionContext : vscode.ExtensionContex
   }
 
   view = helpers.CreateExplorerView("Chromium Explorer", "Chromium Explorer", "media/icon.png");
+  view.setVariable("endpoint", endpoint);
 }
